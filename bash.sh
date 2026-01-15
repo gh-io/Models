15a274ca6f8896a1fd3d616c5e54819725d01f2c
Seriki yakub <sandraisom00@gmail.com>
2026-01-15 17:12:58 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,22 @@
+      npm theme={"system"}
+      # Search API
+      npm install @algolia/client-search
+      # Recommend API
+      npm install @algolia/recommend
+      # Analytics API
+      npm install @algolia/client-analytics
+      # A/B testing API
+      npm install @algolia/client-abtesting
+      # Ingestion API
+      npm install @algolia/ingestion
+      # Insights API
+      npm install @algolia/client-insights
+      # Monitoring
+      npm install @algolia/monitoring
+      # Personalization API
+      npm install @algolia/client-personalization
+      # Query Suggestions API
+      npm install @algolia/client-query-suggestions
+      # Usage API
+      npm install @algolia/client-usage
+    
